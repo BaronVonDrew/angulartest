2a3e94c9c1c103e85b9c6fcf47c55dcffe7810df
Drew Cunningham <atacunningham@users.noreply.github.com>
2021-04-29 23:46:05 +0100

    Very basic answer

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { combineLatest, concat, merge, Observable } from 'rxjs';
+import { combineAll, concatAll, concatMap, map, mergeMap } from 'rxjs/operators';
+import { Feature, FeatureService } from '../service/feature.service';
 
 @Component({
   selector: 'app-feature',
@@ -7,7 +10,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FeatureComponent implements OnInit {
   @Input() userId!: number;
-  
+  private features$!: Observable<Feature[]>;
   /*
   Update the feature component to get and display a list of features per userId
   The component will always receive a userId 
@@ -15,8 +18,16 @@ export class FeatureComponent implements OnInit {
   The feature.service.ts is defined in service/feature.service.ts
   It has two methods, one to get the featureIds for the user and one to get all features.
   */
-  constructor() { }
+
+  //They should inject the service here
+  constructor(private readonly featureService: FeatureService) { }
 
   ngOnInit(): void {
+    this.features$ = combineLatest(
+        this.featureService.getFeatures(),
+        this.featureService.getActiveFeatureIdsForUser(this.userId)
+    ).pipe(
+      map(([features, activeIds]) => features.filter(feature => activeIds.includes(feature.id)))
+    );    
   }
 }
